72812ab2801453242fdc02fada3d17ab0691184a
suzuki_ta@od <st09d18@icloud.com>
2023-07-30 19:04:43 +0900

    add tailwindcss and daisyui

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="text-3xl underline">hoge</h1>
     <NuxtWelcome />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./app.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
+}
+
