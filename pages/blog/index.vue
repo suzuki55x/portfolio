471dcb111b6b0b77b1064387127b23912841592e
suzuki_ta@od <st09d18@icloud.com>
2023-09-13 23:06:01 +0900

    Update とりあえずブログ目次

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,11 +10,21 @@ const { data } = await useFetch("/blogs", {
 </script>
 
 <template>
-  <div class="">
-    <ol>
-      <li v-for="article in data.contents" :key="article.id">
-        <nuxtLink :to="`/blog/${article.id}`">{{ article.title }}</nuxtLink>
-      </li>
-    </ol>
+  <div class="container mx-auto my-16">
+    <div class="flex justify-between flex-auto flex-col sm:flex-row flex-wrap">
+      <template v-for="article in data.contents" :key="article.id">
+        <nuxtLink :to="`/blog/${article.id}`"
+          class="image-full card glass min-h-16 my-4 mx-auto sm:m-4  w-4/5 sm:w-5/12 max-h-48">
+          <figure class=""><img :src="article.eyecatch ? article.eyecatch.url : undefined" alt=""></figure>
+          <div class="h-full flex flex-col justify-between card-body p-4 ">
+            <h2 class="card-title">{{ article.title }}</h2>
+            <div>
+              <div class="text-xs badge badge-neutral">{{ article.category.name ?? "カテゴリなし" }}</div>
+              <div class=" text-sm text-end">{{ article.publishedAt }}</div>
+            </div>
+          </div>
+        </nuxtLink>
+      </template>
+    </div>
   </div>
 </template>
