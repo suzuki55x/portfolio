1a2f6fd8e0bec99b9fc1dcd018379d4772948542
T. Suzuki <st09d18@icloud.com>
2024-03-13 12:45:32 +0900

    Add meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "Takahito Suzuki's portfolio" },
       ],
       htmlAttrs: {
         "data-theme": "lite",
